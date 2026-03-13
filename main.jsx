df3e3680ccafcfe327ca6019f32e67a42b7f93ea
nachogudi04 <nachogudi04@gmail.com>
2026-03-12 21:30:50 -0300

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import WePiper from './WePiper.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <WePiper />
+  </React.StrictMode>
+)
